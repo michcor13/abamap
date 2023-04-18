cb103aacd95ec57c3ab2e0b428da136fde555e60
MichCor <michele.corona@hotmail.com>
2023-04-18 14:38:23 -0600

    Cambios
    Agregue mision vision y empresa, asi como un poco de css

--- resources/views/inicio.blade.php
+++ resources/views/inicio.blade.php
@@ -9,7 +9,7 @@
                           <div class="row">
                             <div class="col-12 col-md-12">
                               <h1 class="headline-resp">
-                                <strong>Bienvenido a la mejor experiencia</strong><br> en materiales de construcción
+                                <strong>Bienvenido a la mejor experiencia</strong><br> en materia prima de alimentos
                               </h1>
                               <p>
                                 {{-- <a class="btn btn-lg btn-second body-2" href="https://wa.me/524424269852">Hablar con un asesor</a> --}}
@@ -23,7 +23,7 @@
                           <div class="row">
                             <div class="col-12 col-md-12">
                               <h1 class="headline-resp">
-                                <strong>Contamos con la mejor tecnología</strong><br> en la fabricación de vibrocomprimidos
+                                <strong>Contamos con la mejor tecnología</strong><br> en la distrubucion de y comercializacion de insumos alimenticios
                               </h1>
                               <p>
                                 {{-- <a class="btn btn-lg btn-second body-2" href="https://wa.me/524424269852">Hablar con un asesor</a> --}}
@@ -46,6 +46,42 @@
                           </div>
                         </div>
                 </div>
+            </div>
+            <div id="misivalo">
+              <span class="tituloemp" >¿Quienes somos?</span>
+              <br>
+              <br>
+              <p class="empresa">
+                  Somos una empresa Mexicana comprometidos con la elaboracion de productos
+                  con la mejor calidad y prestigio.
+                  <br>
+                  Nos hemos mantenido en constante crecimiento en tecnologia, infraestructura e insumos
+                  atendiendo de manera optima las necesidades y la demanda de nuestro mercado.
+              </p>
+              <h1 class="misvisval">
+                Misiòn
+              </h1>
+              <h2 class="empresa">
+              Comercializar materias primas para la industria alimenticia, con los mejores precios y el mejor servicio.
+              </h2>
+              <h1 class="misvisval">
+                Visiòn
+              </h1>
+              <h2 class="empresa">
+              busca ser una empresa lider en su ramo, continuando dando el mejor servicio y satisfaccion a nuestros clientes.
+              </h2>
+              <h1 class="misvisval">
+                Valores
+              </h1>
+              <h2 class="empresa">
+              Integridad
+              <br>
+              Confiabilidad
+              <br>
+              Responsabilidad
+              <br>
+              Humildad.
+              </h2>
             </div>
         </section>
     </body>
