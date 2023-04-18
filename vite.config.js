fcc61ff556e5b8c67e182c5e45b592970ffe68aa
D0bb0 <jonavp@outlook.com>
2023-04-17 23:06:29 -0600

    cambios

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,23 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
+    server: {
+        origin: 'http://127.0.0.1',
+        port: '8000',
+        open: 'http://127.0.0.1:8000'
+    },
     plugins: [
         laravel([
             'resources/css/app.css',
             'resources/js/app.js',
         ]),
     ],
+    css: {
+        preprocessorOptions: {
+          less: {
+            rewriteUrls: 'local',
+            rootpath: "http://localhost:8000/src",
+          },
+        },
+      },
 });
