87ed409725365063d309cdb4cd01466a51e8529c
D0bb0 <jonavp@outlook.com>
2023-05-02 20:32:06 -0600

    (JAVP) 01/05/2023 Se agrega "productos-serviucio y contacto"

--- resources/js/components/MainProductos.jsx
+++ resources/js/components/MainProductos.jsx
@@ -1,39 +1,71 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-function Example() {
+export default function ProductosServicio() {
     return (
-        <div className="container">
-            <div className="row justify-content-center">
-                <div className="col-md-8">
-                    <div className="card">
-                        <div className="card">
-                            <ul className="list-group list-group-flush">
-                                <button type="button" className="list-group-item d-flex justify-content-between align-items-center body-1 color-gray-3 bg-light border-0 list-group-item-action collapsed" aria-expanded="false" aria-controls="adocreto-0">
-                                    Adocreto
-                                    <span className="when-opened"><i className="fas fa-chevron-down color-blue"></i></span> <span className="when-closed"><i className="fas fa-chevron-up color-blue"></i></span></button>
-                                <li className="list-group-item">A second item</li>
-                                <li className="list-group-item">A third item</li>
-                            </ul>
-                            <div className="card-footer">
-                                Card footer
+        <section className="container my-5 py-5">
+            <div className="row">
+                <div className="col-md-4 col-12">
+                    <div className="accordion" id="accordionExample">
+                        <div className="accordion-item">
+                            <h2 className="accordion-header" id="headingOne">
+                                <button className="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+                                    Acidulantes
+                                </button>
+                            </h2>
+                            <div id="collapseOne" className="accordion-collapse collapse show" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
+                                <div className="accordion-body">
+                                    <strong>Acético</strong>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="accordion-item">
+                            <h2 className="accordion-header" id="headingTwo">
+                                <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+                                    Hidrocoloides/Gomas
+                                </button>
+                            </h2>
+                            <div id="collapseTwo" className="accordion-collapse collapse" aria-labelledby="headingTwo" data-bs-parent="#accordionExample">
+                                <div className="accordion-body">
+                                    <strong>Guar</strong>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="accordion-item">
+                            <h2 className="accordion-header" id="headingThree">
+                                <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+                                    Conservadores
+                                </button>
+                            </h2>
+                            <div id="collapseThree" className="accordion-collapse collapse" aria-labelledby="headingThree" data-bs-parent="#accordionExample">
+                                <div className="accordion-body">
+                                    <strong>Benzonato de sodio</strong>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className='col-md-8 col-12'>
+                    <div className='row'>
+                        <div className='col-md-12 col-12'>
+                            <h3 className='text-center'>Acético</h3>
+                            <div className=''>
+                                Ampliamente utilizado en la industria alimenticia por la estabilidad proporcionada en el pH y la acidez en los productos terminados. Por su sabor avinagrado es excelente para conservas y encurtidos.
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
-        </div>
+        </section>
     );
 }
-
-export default Example;
 
 if (document.getElementById('productosServicio')) {
     const Index = ReactDOM.createRoot(document.getElementById("productosServicio"));
 
     Index.render(
         <React.StrictMode>
-            <Example />
+            <ProductosServicio />
         </React.StrictMode>
     )
 }
--- resources/views/contacto.blade.php
+++ resources/views/contacto.blade.php
@@ -1,6 +1,40 @@
 @extends('layouts.app')
 @section('content')
-    <body>
-        Pagina de contacto
-    </body>
+    <section class="container my-5 py-5">
+        <div class="row justify-content-center">
+            <div class="col-md-6 col-12">
+                <h2 class="text-center">Contacta con nosotros</h2>
+                <form>
+                    <div class="mb-3">
+                        <label for="material">Material</label>
+                        <select class="form-select" name="material" id="material">
+                            <option></option>
+                            <option value="Acético">Acético</option>
+                            <option value="Cítrico">Cítrico</option>
+                            <option value="Fumárico">Fumárico</option>
+                            <option value="Láctico">Láctico</option>
+                            <option value="Málico">Málico</option>
+                            <option value="Ascórbico">Ascórbico</option>
+                            <option value="Sórbico">Sórbico</option>
+                            <option value="Tartárico">Tartárico</option>
+                        </select>
+                    </div>
+                    <div class="mb-3">
+                        <label for="email" class="form-label">Correo electrónico</label>
+                        <input type="email" class="form-control" name="emial" id="emial" placeholder="nombre@ejemplo.com">
+                    </div>
+                    <div class="mb-3">
+                        <label for="mensaje" class="form-label">Mensaje</label>
+                        <textarea class="form-control" name="mensaje" id="mensaje" rows="3"></textarea>
+                    </div>
+                    <div class="row row-sm justify-content-center">
+                        <div class="col-4 text-center">
+                            <button type="submit" class="btn btn-primary">Enviar</button>
+                        </div>
+                    </div>
+                </form>
+            </div>
+        </div>
+        </div>
+    </section>
 @stop
