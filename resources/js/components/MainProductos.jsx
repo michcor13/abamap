8fa2f17e054c7dc4dd6dda42cba96a7e933183b7
D0bb0 <jonavp@outlook.com>
2023-05-25 22:56:02 -0600

    Cambios en inicio y productos y servicio.

--- resources/js/components/MainProductos.jsx
+++ resources/js/components/MainProductos.jsx
@@ -1,60 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-
+import { useState } from 'react';
+import AccordionItems from './accordion/accordionItems';
 export default function ProductosServicio() {
+    const [accordionContent, setAccordionContent] = useState([]);
     return (
         <section className="container my-5 py-5">
             <div className="row">
                 <div className="col-md-4 col-12">
-                    <div className="accordion" id="accordionExample">
-                        <div className="accordion-item">
-                            <h2 className="accordion-header" id="headingOne">
-                                <button className="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
-                                    Acidulantes
-                                </button>
-                            </h2>
-                            <div id="collapseOne" className="accordion-collapse collapse show" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
-                                <div className="accordion-body">
-                                    <strong>Acético</strong>
-                                </div>
-                            </div>
-                        </div>
-                        <div className="accordion-item">
-                            <h2 className="accordion-header" id="headingTwo">
-                                <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
-                                    Hidrocoloides/Gomas
-                                </button>
-                            </h2>
-                            <div id="collapseTwo" className="accordion-collapse collapse" aria-labelledby="headingTwo" data-bs-parent="#accordionExample">
-                                <div className="accordion-body">
-                                    <strong>Guar</strong>
-                                </div>
-                            </div>
-                        </div>
-                        <div className="accordion-item">
-                            <h2 className="accordion-header" id="headingThree">
-                                <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
-                                    Conservadores
-                                </button>
-                            </h2>
-                            <div id="collapseThree" className="accordion-collapse collapse" aria-labelledby="headingThree" data-bs-parent="#accordionExample">
-                                <div className="accordion-body">
-                                    <strong>Benzonato de sodio</strong>
-                                </div>
-                            </div>
-                        </div>
+                    <div className="accordion" id="productos">
+                        <AccordionItems setAccordionContent={setAccordionContent}/>
                     </div>
                 </div>
-                <div className='col-md-8 col-12'>
+                {accordionContent.length > 0 ? <div className='col-md-8 col-12' id={accordionContent[0]}>
                     <div className='row'>
                         <div className='col-md-12 col-12'>
-                            <h3 className='text-center'>Acético</h3>
-                            <div className=''>
-                                Ampliamente utilizado en la industria alimenticia por la estabilidad proporcionada en el pH y la acidez en los productos terminados. Por su sabor avinagrado es excelente para conservas y encurtidos.
-                            </div>
+                            <h3 className='text-center'>{accordionContent[0]}</h3>
+                                {accordionContent[1]}
                         </div>
                     </div>
-                </div>
+                </div>:''}
             </div>
         </section>
     );
--- resources/views/inicio.blade.php
+++ resources/views/inicio.blade.php
@@ -1,88 +1,96 @@
 @extends('layouts.app')
 @section('content')
+
     <body>
         <section>
             <div id="main-carousel" class="carousel slide" data-bs-ride="carousel">
                 <div class="carousel-inner">
                     <div class="carousel-item item-1 active">
                         <div class="carousel-caption text-left d-flex h-100 align-items-center">
-                          <div class="row">
-                            <div class="col-12 col-md-12">
-                              <h1 class="headline-resp">
-                                <strong>Bienvenido a la mejor experiencia</strong><br> en materia prima de alimentos
-                              </h1>
-                              <p>
-                                {{-- <a class="btn btn-lg btn-second body-2" href="https://wa.me/524424269852">Hablar con un asesor</a> --}}
-                              </p>
+                            <div class="row">
+                                <div class="col-12 col-md-12">
+                                    <h1 class="headline-resp">
+                                        <strong>Bienvenido a la mejor experiencia</strong><br> en materia prima de alimentos
+                                    </h1>
+                                    <p>
+                                        {{-- <a class="btn btn-lg btn-second body-2" href="https://wa.me/524424269852">Hablar con un asesor</a> --}}
+                                    </p>
+                                </div>
                             </div>
-                          </div>
                         </div>
-                      </div>
-                      <div class="carousel-item item-2">
+                    </div>
+                    <div class="carousel-item item-2">
                         <div class="carousel-caption text-left d-flex h-100 align-items-center">
-                          <div class="row">
-                            <div class="col-12 col-md-12">
-                              <h1 class="headline-resp">
-                                <strong>Contamos con la mejor tecnología</strong><br> en la distrubucion de y comercializacion de insumos alimenticios
-                              </h1>
-                              <p>
-                                {{-- <a class="btn btn-lg btn-second body-2" href="https://wa.me/524424269852">Hablar con un asesor</a> --}}
-                              </p>
+                            <div class="row">
+                                <div class="col-12 col-md-12">
+                                    <h1 class="headline-resp">
+                                        <strong>Contamos con la mejor tecnología</strong><br> en la distrubucion de y
+                                        comercializacion de insumos alimenticios
+                                    </h1>
+                                    <p>
+                                        {{-- <a class="btn btn-lg btn-second body-2" href="https://wa.me/524424269852">Hablar con un asesor</a> --}}
+                                    </p>
+                                </div>
                             </div>
-                          </div>
                         </div>
-                      </div>
-                      <div class="carousel-item item-3">
+                    </div>
+                    <div class="carousel-item item-3">
                         <div class="carousel-caption text-left d-flex h-100 align-items-center">
-                          <div class="row">
-                            <div class="col-12 col-md-12">
-                              <h1 class="headline-resp">
-                                <strong>Amplios inventarios</strong><br> en nuestra gama de productos
-                              </h1>
-                              <p>
-                                {{-- <a class="btn btn-lg btn-second body-2" href="https://wa.me/524424269852">Hablar con un asesor</a> --}}
-                              </p>
+                            <div class="row">
+                                <div class="col-12 col-md-12">
+                                    <h1 class="headline-resp">
+                                        <strong>Amplios inventarios</strong><br> en nuestra gama de productos
+                                    </h1>
+                                    <p>
+                                        {{-- <a class="btn btn-lg btn-second body-2" href="https://wa.me/524424269852">Hablar con un asesor</a> --}}
+                                    </p>
+                                </div>
                             </div>
-                          </div>
                         </div>
+                    </div>
                 </div>
-            </div>
-            <div id="misivalo">
-              <span class="tituloemp" >¿Quienes somos?</span>
-              <br>
-              <br>
-              <p class="empresa">
-                  Somos una empresa Mexicana comprometidos con la elaboracion de productos
-                  con la mejor calidad y prestigio.
-                  <br>
-                  Nos hemos mantenido en constante crecimiento en tecnologia, infraestructura e insumos
-                  atendiendo de manera optima las necesidades y la demanda de nuestro mercado.
-              </p>
-              <h1 class="misvisval">
-                Misiòn
-              </h1>
-              <h2 class="empresa">
-              Comercializar materias primas para la industria alimenticia, con los mejores precios y el mejor servicio.
-              </h2>
-              <h1 class="misvisval">
-                Visiòn
-              </h1>
-              <h2 class="empresa">
-              busca ser una empresa lider en su ramo, continuando dando el mejor servicio y satisfaccion a nuestros clientes.
-              </h2>
-              <h1 class="misvisval">
-                Valores
-              </h1>
-              <h2 class="empresa">
-              Integridad
-              <br>
-              Confiabilidad
-              <br>
-              Responsabilidad
-              <br>
-              Humildad.
-              </h2>
-            </div>
+                <div class="container text-center">
+                    <div class="row col-12 mb-5">
+                        <span class="tituloemp">¿Quienes somos?</span>
+                    </div>
+                    <div class="row col">
+                        <p class="empresa">
+                            Somos una empresa Mexicana comprometidos con la elaboracion de productos
+                            con la mejor calidad y prestigio.
+                            <br>
+                            Nos hemos mantenido en constante crecimiento en tecnologia, infraestructura e insumos
+                            atendiendo de manera optima las necesidades y la demanda de nuestro mercado.
+                        </p>
+                    </div>
+                    <div class="row col">
+                        <h1 class="misvisval">
+                            Misión
+                        </h1>
+                        <h2 class="empresa">
+                            Comercializar materias primas para la industria alimenticia, con los mejores precios y el mejor
+                            servicio.
+                        </h2>
+                        <h1 class="misvisval">
+                            Visión
+                        </h1>
+                        <h2 class="empresa">
+                            busca ser una empresa lider en su ramo, continuando dando el mejor servicio y satisfaccion a
+                            nuestros clientes.
+                        </h2>
+                        <h1 class="misvisval">
+                            Valores
+                        </h1>
+                        <h2 class="list-group">
+                            Integridad
+                            <br>
+                            Confiabilidad
+                            <br>
+                            Responsabilidad
+                            <br>
+                            Humildad.
+                        </h2>
+                    </div>
+                </div>
         </section>
     </body>
 @stop
