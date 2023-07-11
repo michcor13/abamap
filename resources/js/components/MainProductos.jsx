0d98ea9dd90b84892570ad7ad0a637f82a4ec02b
D0bb0 <jonavp@outlook.com>
2023-07-10 21:26:42 -0600

    Cambios en importación de imagenes

--- resources/js/components/MainProductos.jsx
+++ resources/js/components/MainProductos.jsx
@@ -14,11 +14,13 @@ export default function ProductosServicio() {
                     </div>
                 </div>
                 {(<div className='col-md-8 col-12' tabIndex={0} id={accordionContent[0]}>
-                    <div className='row'>
-                        {accordionContent[2]}
-                        <div className='col-md-12 col-12'>
+                    <div className={'row '+ (window.innerWidth > 767 ? 'ms-5': '')}>
+                        <div className='col-md-12 col-12 mt-3'>
                             <h1 className='text-center misvisval'>{accordionContent[0]}</h1>
                                 {accordionContent[1]}
+                        </div>
+                        <div className='text-center my-3'>
+                            {accordionContent[2]}
                         </div>
                     </div>
                 </div>)}
