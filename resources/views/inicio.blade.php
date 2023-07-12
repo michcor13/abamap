e3a15f40071e255751cb292c0e32c1222ab1dccb
D0bb0 <jonavp@outlook.com>
2023-07-11 22:13:07 -0600

    CAmbios de posición por error en producción

--- resources/views/inicio.blade.php
+++ resources/views/inicio.blade.php
@@ -56,7 +56,7 @@
                     <div class="row">
                         <div class="col-12 col-lg-4 align-self-center mt-3">
                             <div class="section-custom-img">
-                                <img src="{{ Vite::asset('resources/image/Pagina/ABAMAP.png') }}" class="img-fluid custom-img">
+                                <img src="{{ asset('image/abamap.png') }}" class="img-fluid custom-img">
                             </div>
                         </div>
                         <div class="col-12 col-lg-8 mt-5 text-center">
